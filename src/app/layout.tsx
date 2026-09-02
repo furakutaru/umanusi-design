09e6cf178017c3943c72696d3d77889dce06f919
furakutaru <ec.yuma@gmail.com>
2026-09-02 09:27:13 +0900

    チャット機能導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,20 @@ export default function RootLayout({
         `}
         </Script>
         {children}
+        {/* Tawk.to Live Chat */}
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6a976ca9a5d272343eb98e9b/1k1fo0tos';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
